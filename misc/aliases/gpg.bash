217fb27fb907ce71722933522c2f7e7926cf8dfd
Viacheslav Lotsmanov <lotsmanov89@gmail.com>
2021-03-17 02:10:38 +0200

    Refactor ‘tmpgpg’ alias-ish (function)
    1. Move usage info from comments into the command (available by ‘--help’)
    2. Use ‘set -Eeuo pipefail’

--- misc/aliases/gpg.bash
+++ misc/aliases/gpg.bash
@@ -1,59 +1,102 @@
 #! /usr/bin/env bash
 
-# $1 - encrypted file
-#
-# $2 - a shell command to do something with encrypted file where
-#      $f is the encrypted filename (from $1 argument),
-#      $d is the directory of original $1 file,
-#      $t is new temporary directory where decrypted file $f is saved,
-#      working directory is changed to $t so you are free to mess there and all
-#      this will be cleaned up when this shell command is done.
-#
-# [$3] - optional [--silent|-s] flag which silents boilerplate noise
-#        but keeps stderr output inside shell command from $2 argument
-#
-tmpgpg() {
-	if (( $# < 2 || $# > 3 )) || [[ -z $1 || ! -f $1 ]]; then
-		>&2 echo \
-			Incorrect arguments! Provide encrypted file and a shell command!
+# Call with '--help' to see the usage info
+tmpgpg() (
+	set -Eeuo pipefail || exit
+
+	show-usage() {
+		set -Eeuo pipefail || exit
+
+		local USAGE_INFO; declare -A USAGE_INFO=(
+			['1|ENCRYPTED_FILE']='Encrypted file'
+			['2|SHELL_COMMAND']='
+				A shell command to do something with encrypted file where
+				$f is encrypted filename (from ENCRYPTED_FILE argument),
+				$d is directory of original ENCRYPTED_FILE,
+				$t is temporary directory where decrypted $f is saved to.
+				Working directory is changed to $t so that you are free to
+				mess there and all this will be cleaned up when this shell
+				command is done.
+				'
+			['3|[OPTION]']='
+				Optional [--silent|-s] flag which silents boilerplate noise
+				but keeps stderr output inside shell command from
+				SHELL_COMMAND argument
+				'
+		)
+
+		local args; args=$(printf '%s\n' "${!USAGE_INFO[@]}" | sort)
+		readarray -t args <<< "$args"
+		printf '\nUsage: %s %s\n\n' "${FUNCNAME[1]}" "${args[*]//[0-9]|/}"
+		printf 'Arguments:\n\n'
+		local arg_name spacer arg_printed arg_description
+		for arg_name in "${args[@]}"; do
+			arg_printed='              '
+			arg_description=$(
+				sed '1{/^\s*$/d}' <<< "${USAGE_INFO[$arg_name]}" \
+					| sed -e '1{s/^\s\+//};' -e '${/^\s\+$/d}' \
+						-e "s/^\s\+/  $arg_printed  /"
+			)
+			arg_name=${arg_name//[0-9]|/}
+			arg_name=${arg_name//[\[\]]/}
+			arg_printed=${arg_name}${arg_printed:${#arg_name}}
+			printf '  %s  %s\n\n' "$arg_printed" "$arg_description"
+		done
+	}
+
+	if (( $# == 1 )) && [[ $1 == --help || $1 == '-h' ]]; then
+		show-usage
+		return 0
+	elif (( $# < 2 || $# > 3 )) || [[ -z $1 || ! -f $1 ]]; then
+		>&2 echo Incorrect arguments!
+		show-usage
 		return 1
 	fi
-	local FILE_DIR; FILE_DIR=$(dirname -- "$1") || return
-	local ENCRYPTED_FILE; ENCRYPTED_FILE=$(basename -- "$1"); shift || return
-	local CMD; CMD=$1; shift || return
+
+	local FILE_DIR; FILE_DIR=$(dirname -- "$1")
+	local ENCRYPTED_FILE; ENCRYPTED_FILE=$(basename -- "$1"); shift
+	local CMD; CMD=$1; shift
+
 	local OPT IS_SILENT=NO; if (( $# > 0 )); then
-		OPT=$1; shift || return
+		OPT=$1; shift
 		IS_SILENT=$(
+			set -Eeuo pipefail || exit
 			if [[ $OPT == '-s' || $OPT == --silent ]];
 			then echo YES;
 			else ( >&2 printf 'Unexpected option: "%s"\n' "$OPT" && return 1 )
 			fi
-		) || return
+		)
 	fi
+
 	if (( $# != 0 )); then
-		>&2 echo "Some ($#) arguments left unparsed!"
+		>&2 printf 'Some (%d) arguments left unparsed!' "$#"
 		return 1
 	fi
+
 	(
-		cd -- "$FILE_DIR" || return
-		local FILE_DIR_PWD; FILE_DIR_PWD=$PWD || return
-		local TMPDIR; TMPDIR=$(mktemp -d --suffix="-$ENCRYPTED_FILE") || return
+		cd -- "$FILE_DIR"
+		local FILE_DIR_PWD; FILE_DIR_PWD=$PWD
+		local TMPDIR; TMPDIR=$(mktemp -d --suffix="-$ENCRYPTED_FILE")
+
 		local CLEANUP; CLEANUP=$(
+			set -Eeuo pipefail || exit
 			if [[ $IS_SILENT == YES ]]; then echo -n 'exec 2>/dev/null ;'; fi
 			echo -n $'find "$TMPDIR/" -type f -exec shred -vufz -n10 {} \;'
 			echo -n ';find "$TMPDIR/" -type d | tac | xargs rmdir'
-		) || return
-		trap -- "$CLEANUP" EXIT || return
+		)
+
+		trap -- "$CLEANUP" EXIT
 		(
 			if [[ $IS_SILENT == YES ]]; then exec 2>/dev/null; fi
-			gpg -d -o "$TMPDIR/$ENCRYPTED_FILE" -- "$ENCRYPTED_FILE" || return
+			gpg -d -o "$TMPDIR/$ENCRYPTED_FILE" -- "$ENCRYPTED_FILE"
 		)
-		cd -- "$TMPDIR" || return
+		cd -- "$TMPDIR"
 		if [[ $IS_SILENT == NO ]]; then
 			>&2 echo \
 				'~~~~~~~~~~~~~~~~ RUNNING THE SHELL COMMAND ~~~~~~~~~~~~~~~~~'
 		fi
 		local SHELL_CMD; SHELL_CMD=$(
+			set -Eeuo pipefail || exit
 			echo 'if [[ -n $BASH_DIR_PLACEHOLDER ]]; then'
 			echo '  . "$BASH_DIR_PLACEHOLDER"/.bash_aliases || exit'
 			echo 'elif [[ -f ~/.bash_aliases ]]; then'
@@ -61,12 +104,11 @@ tmpgpg() {
 			echo 'fi'
 			echo 'set -x || exit'
 			printf %s "$CMD"
-		) || return
-		f=$ENCRYPTED_FILE d=$FILE_DIR_PWD t=$TMPDIR \
-			"$SHELL" -c "$SHELL_CMD" || return
+		)
+		f=$ENCRYPTED_FILE d=$FILE_DIR_PWD t=$TMPDIR "$SHELL" -c "$SHELL_CMD"
 		if [[ $IS_SILENT == NO ]]; then
 			>&2 echo \
 				'~~~~~~~~~~~~~~~~~~~~~~~~~~~ DONE ~~~~~~~~~~~~~~~~~~~~~~~~~~~'
 		fi
 	)
-}
+)
