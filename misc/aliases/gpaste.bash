21c4330a158489aa02bc87b1d91669c269a5cb30
Viacheslav Lotsmanov <lotsmanov89@gmail.com>
2022-02-07 00:09:44 +0200

    Add “suspend-gp” command to GPaste misc aliases

--- misc/aliases/gpaste.bash
+++ misc/aliases/gpaste.bash
@@ -2,7 +2,7 @@
 # Author: Viacheslav Lotsmanov
 # License: MIT https://raw.githubusercontent.com/unclechu/bashrc/master/LICENSE
 
-# shortcut for gpaste cli
+# Shortcut for GPaste CLI
 alias gp=$(
 	if   [[ -x $(type -P gpaste-client 2>/dev/null) ]]; then echo 'gpaste-client'
 	elif [[ -x $(type -P gpaste        2>/dev/null) ]]; then echo 'gpaste'
@@ -12,3 +12,10 @@ alias gp=$(
 
 # Copy to system clipboard without any line breaks (“i” for “inline”).
 alias gpi='perl -pe chomp | gp'
+
+# Suspend GPaste temporarily until current program exits.
+# For instance can be useful when copying and pasting password, to avoid them
+# getting into the clipboard history.
+# Can be used in a subprogram like this:
+#   (suspend-gp && keepassx)
+alias suspend-gp='trap "gp start; gp get --use-index 0 | gp" EXIT && gp stop'
