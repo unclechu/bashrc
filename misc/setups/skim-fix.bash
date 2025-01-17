b97dd62497d27538f998d45dc08d12c6e8fb9531
Viacheslav Lotsmanov <lotsmanov89@gmail.com>
2025-01-17 10:37:55 +0200

    Add spaces at the end for custom ^R & ^T bindings

--- misc/setups/skim-fix.bash
+++ misc/setups/skim-fix.bash
@@ -14,25 +14,26 @@
 # bind -m vi-insert '"\C-t": "\C-z\C-t\C-z"'
 # My own customized/simplified “__skim_select__” implementation
 __my_skim_select__() {
-	local cmd="${SKIM_CTRL_T_COMMAND:-"command find -L . -mindepth 1 \\( -path '*/\\.*' -o -fstype 'sysfs' -o -fstype 'devfs' -o -fstype 'devtmpfs' -o -fstype 'proc' \\) -prune -o -type f -print -o -type d -print -o -type l -print 2> /dev/null | cut -b3-"}";
+	local cmd
+	cmd="${SKIM_CTRL_T_COMMAND:-"command find -L . -mindepth 1 \\( -path '*/\\.*' -o -fstype 'sysfs' -o -fstype 'devfs' -o -fstype 'devtmpfs' -o -fstype 'proc' \\) -prune -o -type f -print -o -type d -print -o -type l -print 2> /dev/null | cut -b3-"}"
 
-	local sk_cmd=
+	local sk_cmd
 	if [[ ! -v NO_TMUX_F || -z $NO_TMUX_F ]] && [[ -v TMUX && -n $TMUX ]]; then
 		sk_cmd=sk-tmux
 	else
 		sk_cmd=sk
 	fi
 
-	eval "$cmd" | "$sk_cmd" | while read -r item; do printf '%q ' "$item"; done
+	eval -- "$cmd" | "$sk_cmd" | while read -r x; do printf %q "$x"; done
 	echo
 }
-bind -m vi-insert '"\C-t": "\C-u$(__my_skim_select__)\C-x\C-e"'
-bind -m vi-command '"\C-t": "\C-u$(__my_skim_select__)\C-x\C-e"'
+bind -m vi-insert '"\C-t": "\C-u$(__my_skim_select__)\C-x\C-e "'
+bind -m vi-command '"\C-t": "\C-u$(__my_skim_select__)\C-x\C-e "'
 
 # CTRL-R - Paste the selected command from history into the command line
 # bind -m emacs-standard '"\C-r": "\C-e \C-u\C-y\ey\C-u"$(__skim_history__)"\e\C-e\er"'
 # bind -m vi-command '"\C-r": "\C-z\C-r\C-z"'
 # bind -m vi-insert '"\C-r": "\C-z\C-r\C-z"'
 # WARNING! Depends on the “hf” alias (see misc/aliases/skim.bash)
-bind -m vi-insert '"\C-r": "\C-u$(hf)\C-x\C-e"'
-bind -m vi-command '"\C-r": "\C-u$(hf)\C-x\C-e"'
+bind -m vi-insert '"\C-r": "\C-u$(hf)\C-x\C-e "'
+bind -m vi-command '"\C-r": "\C-u$(hf)\C-x\C-e "'
